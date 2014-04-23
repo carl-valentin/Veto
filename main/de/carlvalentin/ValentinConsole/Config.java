7d5f2e3d0dc9498b5fcd18af4b11c6eaeecaf3f2
benjamin_ti <b.tisler@gmx.de>
2014-04-23 07:43:07 +0000

    P0.3
    git-svn-id: https://cvsvn/svn/Veto/branches/P0.3@11 e3be303d-166c-2441-b441-369fcf21e0a0

---
+++ main/de/carlvalentin/ValentinConsole/Config.java
@@ -0,0 +1,175 @@
+package de.carlvalentin.ValentinConsole;
+
+import java.io.*;
+import java.util.*;
+import javax.swing.JOptionPane;
+
+/**
+ * Speicher Konfigurationsdaten in einer "Cookie"-Datei im Temp-Verzeichniss
+ * des Benutzers.
+ */
+public class Config {
+    private final String strConfigVersionID = "Cfg-File Version";
+    private final String strConfigVersion = "0.1";
+    
+    private String strConfigFileName;
+    private List list = Collections.synchronizedList(new LinkedList());
+    
+    private class Config_Element {
+    	public String strID;
+        public String strValue;
+        
+        private Config_Element() {}
+        public  Config_Element(String _strID, String _strValue) {
+        	strID = _strID;
+            strValue = _strValue;
+        }
+    }
+    
+    private void writeError(String s) {
+        try {
+            JOptionPane.showMessageDialog(null, s, "Error",
+                                          JOptionPane.ERROR_MESSAGE);
+        }
+        catch (java.awt.HeadlessException ex) {
+            System.err.println(ex);
+        }
+    }    
+
+    private boolean checkVersion(String strVersionID, String strVersion) {
+        String strVersionInFile;
+        
+        strVersionInFile = getConfig(strVersionID);
+        if (strVersionInFile == null) {
+        	return false;
+        }
+        else {
+            if (strVersion.compareTo(strVersionInFile) != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+	private Config() {
+    }
+
+    /**
+     * @param strName Name der "Cookie"-/Konfigurationsdatei
+     * @param strVersion Version der Datei. Stimmt die hier &uuml;bergebene 
+     *                   Version nicht mit der in der g&ouml;öffneten Datei 
+     *                   &uuml;berein, dann wird der Inhalt ignoriert und 
+     *                   die Datei mit einer neuen &uuml;berschrieben. 
+     */
+    public Config(String strName, String strVersion) {
+        strConfigFileName = strName + ".cfg";
+        
+        try {
+            FileReader fileConfig = 
+                new FileReader(System.getProperty("java.io.tmpdir",".")
+                               + strConfigFileName);
+            StringBuffer inBuf = new StringBuffer();
+            String strID = null;
+            int i;
+            char c;
+            
+            String strVersionInFile;
+
+            while ( (i = fileConfig.read()) > 0 ) {
+                c = (char)i;
+                if (c == '|') {
+                    strID = inBuf.toString();
+                    inBuf = new StringBuffer();
+                }
+                else if (c == '\n') {
+                    Config_Element cfgElement = 
+                        new Config_Element(strID, inBuf.toString());
+                    list.add(cfgElement);
+                    inBuf = new StringBuffer();
+                }
+                else {
+                	inBuf.append(c);
+                }
+            }
+            
+            fileConfig.close();
+        }
+        catch (FileNotFoundException ex) {
+        }
+        catch (IOException ex) {
+            writeError("I/O error while trying to read from file" + 
+                       strConfigFileName);
+        }
+        
+        // Wenn Versionen nicht uebereinstimmen, eingelesene Daten 
+        // wegwerfen und mit Defaultwerten ueberschreiben
+        if (!checkVersion(strName, strVersion) || 
+            !checkVersion(strConfigVersionID, strConfigVersion)    ) {
+            list = Collections.synchronizedList(new LinkedList());
+            setConfig(strName, strVersion);
+            setConfig(strConfigVersionID, strConfigVersion);
+        }        
+    }    
+    
+    /**
+     * Liest den Wert zur ID <code>strID</code> aus der 
+     * "Cookie"-/Konfigurationsdatei ein.
+     * @return <code>null</code> falls die ID noch nicht in der Datei 
+     *         gefunden wurde, sonst den Wert als String
+     */
+    public String getConfig(String strID) {     
+        Object objElement[] = list.toArray();
+        for (int i=0; i<list.size(); i++) {
+        	Config_Element cfgElement = (Config_Element)objElement[i];
+            if (strID.compareTo(cfgElement.strID) == 0) {
+                return cfgElement.strValue;
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * Speichert den Wert <code>strValue</code> in der 
+     * "Cookie"-/Konfigurationsdatei unter der ID <code>strID</code>. 
+     * M.H. der ID kann man den Wert mittels {@link #getConfig} 
+     * wieder einlesen.
+     */
+    public boolean setConfig(String strID, String strValue)
+    {
+        Object objElement[] = list.toArray();
+        for (int i=0; i<list.size(); i++) {
+            Config_Element cfgElement = (Config_Element)objElement[i];
+            if (strID.compareTo(cfgElement.strID) == 0) {
+            	cfgElement.strValue = strValue;
+                writeConfig();
+                return true;
+            }
+        }
+        
+        Config_Element cfgElement = 
+            new Config_Element(strID, strValue);
+        list.add(cfgElement);        
+        writeConfig();
+        return true;
+    }
+    
+    private void writeConfig() {       
+        try {     
+        	FileWriter fileConfig = 
+        		new FileWriter(System.getProperty("java.io.tmpdir",".")
+        				       + strConfigFileName);
+            Object objElement[] = list.toArray();
+            for (int i=0; i<list.size(); i++) {
+                Config_Element cfgElement = (Config_Element)objElement[i];
+                fileConfig.write(cfgElement.strID + "|" + 
+                                 cfgElement.strValue + "\n");
+            }            
+        	fileConfig.close();
+        }
+        catch (IOException ex) {
+            writeError("I/O error while trying to write to file" + 
+                    strConfigFileName);
+        }
+    }
+}
---
+++ main/de/carlvalentin/ValentinConsole/SohEtb.java
@@ -0,0 +1,125 @@
+package de.carlvalentin.ValentinConsole;
+
+import java.util.*;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * SOH/ETB Enum-Klasse zur Dartellung der Start/Stop-Zeichen der 
+ * Carl Valentin Printer Language (CVPL).<br>
+ * <br>
+ * Aufrufbeispiel:
+ * <pre>
+ *     SohEtb sohEtb = SohEtb.x5E5F;     // Start/Stop-Zeichen 5E/5F
+ *     int iSoh = sohEtb.gl_iSOH;        // iSoh = 0x5E
+ *     String strEtb = sohEtb.gl_strETB; // strEtb = "_"
+ * </pre>
+ */
+public final class SohEtb {
+	public final String gl_strSOH;
+	public final String gl_strETB;
+	public final int  gl_iSOH;
+	public final int  gl_iETB;
+	private String id;
+	public final int ord;
+	private SohEtb prev;
+	private SohEtb next;
+
+	private static int upperBound = 0;
+	private static SohEtb first = null;
+	private static SohEtb last = null;
+    
+	private SohEtb(byte bSOH, byte bETB) {
+		byte[] baSOH = new byte[1]; 
+		byte[] baETB = new byte[1];
+		String strSOH;
+		String strETB;
+		
+		baSOH[0] = bSOH; 
+		baETB[0] = bETB;		
+		this.gl_iSOH = bSOH;
+		this.gl_iETB = bETB;
+		try {
+			strSOH = new String(baSOH, "US-ASCII");
+			strETB = new String(baETB, "US-ASCII");
+		}		
+		catch (UnsupportedEncodingException ex) {
+			System.err.println("Encoding US-ASCII is not supported " + 
+			                   "- try to encode with default encoding");
+			strSOH = new String(baSOH);
+			strETB = new String(baETB);				
+		}
+		this.gl_strSOH = strSOH;
+		this.gl_strETB = strETB;
+		this.id = gl_strSOH + "/" + gl_strETB;
+	  	this.ord = upperBound++;
+	  	if (first == null) first = this;
+	  	if (last != null) {
+			this.prev = last;
+			last.next = this;
+	  	}
+	  	last = this;
+	}
+    
+	public static Enumeration elements() {
+		return new Enumeration() {
+			private SohEtb curr = first;
+			public boolean hasMoreElements() {
+		  		return curr != null;
+			}
+			public Object nextElement() {
+		  		SohEtb c = curr;
+		  		curr = curr.next();
+		  		return c;
+			}
+	 	};
+	}
+    
+	public String toString() {return this.id; }
+    
+    /**
+     * Liefert den Wert &uuml;ber die von toString zur&uuml;ckgegebene
+     * Stringrepresentation:
+     * <pre>
+     *     SohEtb sohEtb = SohEtb.x0117;           // sohEtb hat jetzt
+     *                                             // den Wert 01/17
+     *     String str5E5F = SohEtb.x5E5F.toString;
+     *     sohEtb = SohEtb.fromString(str5E5F);    // sohEtb hat jetzt 
+     *                                             // den Wert 5E/5F
+     *     }
+     * </pre>
+     */
+    public static SohEtb fromString(String str)
+    {
+        if (str == null) return null;
+        
+        if (str.compareTo(SohEtb.x0117.toString()) == 0)
+        {            
+        	return SohEtb.x0117;
+        }
+
+        if (str.compareTo(SohEtb.x5E5F.toString()) == 0)
+        {           
+            return SohEtb.x5E5F;
+        }
+        
+        if (str.compareTo(SohEtb.none.toString()) == 0)
+        {
+            return SohEtb.none;
+        }
+        
+        return null;
+    }
+    
+	public static int size() { return upperBound; }
+	public static SohEtb first() { return first; }
+	public static SohEtb last()  { return last;  }
+	public SohEtb prev()  { return this.prev; }
+	public SohEtb next()  { return this.next; }
+	
+	public static final SohEtb x0117 = 
+		new SohEtb((byte)0x01, (byte)0x17);
+	public static final SohEtb x5E5F = 
+		new SohEtb((byte)0x5E, (byte)0x5F);
+    public static final SohEtb none =
+        new SohEtb((byte)0xFF, (byte)0xFF);
+}
